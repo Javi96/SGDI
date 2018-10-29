23b8dfd12f60783dd217a73e33d11fba91f1f166
Javi96 <javi.tejada96@gmail.com>
2018-10-29 20:37:26 +0100

    Agrupados elementos por atributo

--- Práctica 2/ID3/ID3.py
+++ Práctica 2/ID3/ID3.py
@@ -3,50 +3,55 @@ import json
 import csv
 import sys
 import operator
-
+import random
+from termcolor import colored
 class ID3Tree():
 
 	def __init__(self, data, attributes):
-		#print(data, attributes)
-
-		''' ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
-		['vhigh', 'low', '5more', '2', 'big', 'high', 'unacc']
-		['low', 'vhigh', '3', 'more', 'med', 'low', 'unacc']
-		['vhigh', 'low', '5more', '2', 'med', 'low', 'unacc']
-		['vhigh', 'high', '3', 'more', 'small', 'low', 'unacc']
-		['med', 'low', '4', '4', 'big', 'low', 'unacc']
-		['vhigh', 'high', '2', '2', 'med', 'low', 'unacc']
-		['med', 'med', '3', 'more', 'small', 'high', 'acc']
-		['high', 'low', '5more', '2', 'small', 'high', 'unacc']
-		['med', 'vhigh', '4', 'more', 'med', 'med', 'acc']
-		['vhigh', 'vhigh', '4', '4', 'small', 'med', 'unacc']
-		['low', 'med', '5more', 'more', 'med', 'high', 'vgood']
-		['high', 'high', '2', '4', 'med', 'low', 'unacc']
-		['high', 'med', '2', '4', 'small', 'high', 'acc']
-		['low', 'vhigh', '4', '4', 'big', 'med', 'acc']
-		'''
 		self.attributes = attributes
 		self.data = data
+		major_class, count_class = self.get_major_class()
+		self.generate_tree(major_class, count_class)
 
 	def get_major_class(self):
 		classes = {}
 		for instance in self.data:
-			class_value = instance[-1]
+			class_value = instance.get('class')
 			if class_value in classes:
 				classes[class_value] = 1 + classes[class_value]
 			else:
 				classes[class_value] = 1
+
 		for i in classes.items():
 			print(i)
-
 		return max(classes.items(), key=operator.itemgetter(1))[0], len(classes)
 
-	def generate_tree(self):
-		major_class, count_class = self.get_major_class()
-		if count_class == 1 or len(self.attributes) == 0: 
-			print("new hoja(max(classes))")
-		else: print('bifurcamos\nmajor class: ', major_class)
+	def group_by_class(self):
+		result = {}
+		for attribute in self.attributes:
+			grouped_instances = {}
+			print(colored(attribute, 'red'))
+			for instance in self.data:
+				if instance[attribute] in grouped_instances:
+					grouped_instances[instance[attribute]] = [instance] + grouped_instances[instance[attribute]]
+				else:
+					grouped_instances[instance[attribute]] = [instance]
+			result[attribute] = grouped_instances
 
+		return result
+
+	def generate_tree(self, major_class, count_class):
+		if count_class == 1 or len(self.attributes) == 0:
+			print("new hoja(max(classes))")
+		else: 
+			grouped_instances = self.group_by_class() # agrupamos por clase
+			entropy_by_group = self.get_entropy(grouped_instances)
+			#print(colored(json.dumps(grouped_instances, indent=4, sort_keys=True), 'yellow'))
+
+	def get_entropy(self, grouped_instances):
+		for group in grouped_instances.items():
+			print(colored(json.dumps(group, indent=4, sort_keys=True), 'blue'))
+		return 0
 
 
 
@@ -54,15 +59,13 @@ class ID3(object):
 
     def __init__(self, fichero):
     	attributes = []
-    	data = [] 
+    	data = []
     	with open(fichero) as input_file:
-    		line = csv.reader(input_file, delimiter=',')
+    		line = csv.reader(input_file, delimiter = ',')
     		attributes = next(line)
-    		print(attributes)
     		for word in line:
-    			data.append(word)
-    			print(word)
-    	self.tree = ID3Tree(data, attributes)
+    			data.append({attributes[i] : word[i] for i in range(0, len(word))})
+    		self.tree = ID3Tree(data, attributes)
 
 
     def clasifica(self, instancia):
@@ -79,4 +82,4 @@ class ID3(object):
 	
 
 if __name__ == '__main__':
-	ID3(sys.argv[1]).tree.generate_tree()
+	ID3(sys.argv[1])
